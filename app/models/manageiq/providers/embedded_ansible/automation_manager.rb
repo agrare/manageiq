ab6e06f512e84c569b46205498c738ca7df4fe66
Adam Grare <adam@grare.com>
2024-04-10 13:03:53 -0400

    Move generic_ansible_playbook into embedded_ansible
    The catalog_types should live on the EmbeddedAnsible::AutomationManager
    not the base EmbeddedAutomationManager which is inherited by
    Workflows::AutomationManager and EmbeddedTerraform::AutomationManager

--- app/models/manageiq/providers/embedded_ansible/automation_manager.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager.rb
@@ -1,5 +1,10 @@
 class ManageIQ::Providers::EmbeddedAnsible::AutomationManager < ManageIQ::Providers::EmbeddedAutomationManager
   supports_not :refresh_ems
+  supports     :catalog
+
+  def self.catalog_types
+    {"generic_ansible_playbook" => N_("Ansible Playbook")}
+  end
 
   def self.ems_type
     @ems_type ||= "embedded_ansible_automation".freeze
